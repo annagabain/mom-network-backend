cb5a99efe554ec7f15514353eb6290c870f771b1
Anna Gabain <anna.gabain@outlook.com>
2023-08-31 14:50:27 +0000

    Debug profiles

--- api_for_mom/settings.py
+++ api_for_mom/settings.py
@@ -35,8 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+# DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
