d06291abbfff02f4a65ecb3f6c5948204275a6f1
Anna Gabain <anna.gabain@outlook.com>
2023-09-02 15:44:41 +0000

    debug dev off

--- api_for_mom/settings.py
+++ api_for_mom/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
