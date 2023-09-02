c467c2a4912a332a8a7c26c832a0b4660be21d15
Anna Gabain <anna.gabain@outlook.com>
2023-09-02 16:17:07 +0000

    Edit followed_name

--- followers/models.py
+++ followers/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Follower(models.Model):
-    owner = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
-    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
--- followers/serializers.py
+++ followers/serializers.py
@@ -1,22 +1,20 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Follower
-from django.db import IntegrityError
 
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    person_followed_name = serializers.ReadOnlyField(
-        source='followed.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
         model = Follower
-        fields = ['id', 'owner', 'created_at',
-                  'followed', 'person_followed_name']
-    
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
+        ]
+
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({
-                'detail': 'possible duplicate, you have already followed this person!'
-            })
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
